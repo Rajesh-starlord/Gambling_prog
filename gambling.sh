19325fa3c15b5e9a02faac6e3f3ca61d370fe79f
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-06-08 13:54:03 +0530

    added uc-3

--- gambling.sh
+++ gambling.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash 
 #gambling
 
 win=0
@@ -8,17 +8,19 @@ maxwin=150
 loss=0
 maxloss=50
 minbal=$(($totalstock-$maxloss))
+while [ $totalstock -lt $maxwin ] && [ $totalstock -gt $minbal ]
+do
+        rno=$(($RANDOM%3))
 
-rno=$(($RANDOM%3))
+        if [ $rno -eq 1 ]; then
+            win=$(($win+1))
+			betno=$(($betno+1))
+			totalstock=$(($totalstock+1))
+        elif [ $rno -eq 2 ]; then
+     		betno=$(($betno+1))
+			loss=$(($loss+1))
+            totalstock=$(($totalstock-1))
+        fi
+done
 
-if [ $rno -eq 1 ]; then
-	echo "you won"
-	win=$(($win+1))
-	totalstock=$(($totalstock+1))
-elif [ $rno -eq 2 ]; then
-	echo "you lost"
-	loss=$(($loss+1))
-	totalstock=$(($totalstock-1))
-fi
-
-
+echo "you won $win times and loose $loss time || totalbalance= $totalstock"
