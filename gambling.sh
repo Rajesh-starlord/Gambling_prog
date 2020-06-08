d63289655cfc8b7cf78ff4d0e61756f0caa2b5b9
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-06-08 13:51:00 +0530

    added uc-2

--- gambling.sh
+++ gambling.sh
@@ -1,4 +1,4 @@
-#! /bin/bash 
+#! /bin/bash
 #gambling
 
 win=0
@@ -8,3 +8,17 @@ maxwin=150
 loss=0
 maxloss=50
 minbal=$(($totalstock-$maxloss))
+
+rno=$(($RANDOM%3))
+
+if [ $rno -eq 1 ]; then
+	echo "you won"
+	win=$(($win+1))
+	totalstock=$(($totalstock+1))
+elif [ $rno -eq 2 ]; then
+	echo "you lost"
+	loss=$(($loss+1))
+	totalstock=$(($totalstock-1))
+fi
+
+
