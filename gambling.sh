4ad283c4b2cf60edb4489ae6afe26c76faa31fc8
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-06-08 14:05:28 +0530

    added uc-4

--- gambling.sh
+++ gambling.sh
@@ -8,19 +8,27 @@ maxwin=150
 loss=0
 maxloss=50
 minbal=$(($totalstock-$maxloss))
-while [ $totalstock -lt $maxwin ] && [ $totalstock -gt $minbal ]
-do
-        rno=$(($RANDOM%3))
+totaldaystoplay=20
+daycount=0
 
-        if [ $rno -eq 1 ]; then
-            win=$(($win+1))
+while [ $daycount -lt $totaldaystoplay ]
+do
+	while [ $totalstock -lt $maxwin ] && [ $totalstock -gt $minbal ]
+	do
+		rno=$(($RANDOM%3))
+
+		if [ $rno -eq 1 ]; then
+			win=$(($win+1))
 			betno=$(($betno+1))
 			totalstock=$(($totalstock+1))
-        elif [ $rno -eq 2 ]; then
-     		betno=$(($betno+1))
+		elif [ $rno -eq 2 ]; then
+			betno=$(($betno+1))
 			loss=$(($loss+1))
-            totalstock=$(($totalstock-1))
-        fi
-done
+			totalstock=$(($totalstock-1))
+		fi
+	done
 
-echo "you won $win times and loose $loss time || totalbalance= $totalstock"
+	echo "day:$(($daycount+1)) -->you won $win times and loose $loss time || totalbalance= $totalstock"
+	daycount=$(($daycount+1))
+	totalstock=100
+done
