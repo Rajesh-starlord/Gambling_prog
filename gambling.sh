27c0d18ccc3d86710411d9ad9879b22fc14373d5
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-06-08 14:22:41 +0530

    added uc-5

--- gambling.sh
+++ gambling.sh
@@ -3,32 +3,43 @@
 
 win=0
 betno=0
-totalstock=100
+totalStock=100
 maxwin=150
 loss=0
 maxloss=50
-minbal=$(($totalstock-$maxloss))
-totaldaystoplay=20
-daycount=0
+minbal=$(($totalStock-$maxloss))
+totalDaysToPlay=20
+dayCount=0
+winCountPerDay=0
+lostCountPerDay=0
 
-while [ $daycount -lt $totaldaystoplay ]
+while [ $dayCount -lt $totalDaysToPlay ]
 do
-	while [ $totalstock -lt $maxwin ] && [ $totalstock -gt $minbal ]
+	while [ $totalStock -lt $maxwin ] && [ $totalStock -gt $minbal ]
 	do
 		rno=$(($RANDOM%3))
 
 		if [ $rno -eq 1 ]; then
 			win=$(($win+1))
 			betno=$(($betno+1))
-			totalstock=$(($totalstock+1))
+			totalStock=$(($totalStock+1))
 		elif [ $rno -eq 2 ]; then
 			betno=$(($betno+1))
 			loss=$(($loss+1))
-			totalstock=$(($totalstock-1))
+			totalStock=$(($totalStock-1))
 		fi
 	done
-
-	echo "day:$(($daycount+1)) -->you won $win times and loose $loss time || totalbalance= $totalstock"
-	daycount=$(($daycount+1))
-	totalstock=100
+	
+	if [ $totalStock -eq 150 ]; then
+		echo "day:$(($dayCount+1)) --> you won $win times and loose $loss times || totalbalance = $totalStock|| result : Won"
+		winCountPerDay=$(($winCountPerDay+1))
+	elif [ $totalStock -eq 50 ]; then
+		echo "day:$(($dayCount+1)) --> you won $win times and loose $loss times || totalbalance = $totalStock|| result : Lost"
+		lostCountPerDay=$(($lostCountPerDay+1))
+	fi
+	dayCount=$(($dayCount+1))
+	totalStock=100
 done
+
+echo "--------------------------------------------------------------------------------------"
+echo "This Month -->you won $winCountPerDay times and loose $lostCountPerDay times"
